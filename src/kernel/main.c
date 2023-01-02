9f7110fffe5aa67a394e512f952e9412de6564c8
ally <y13822509112@126.com>
2023-01-02 14:01:35 +0800

    Added assertion interface.

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -5,16 +5,14 @@
 #include "../include/ynix/console.h"
 #include "../include/ynix/stdarg.h"
 #include "../include/ynix/printk.h"
+#include "../include/ynix/assert.h"
 
 void kernel_init()
 {
     console_init();
-
-    int cnt = 30;
-    while (cnt--)
-    {
-        printk("hello onix %#010x\n", cnt);
-    }
+    
+    assert(1 > 0);
+    assert(0 > 1);
 
     return;
 }
