1793d3d11a13bbc8848105b0562f85ef5077230e
ally <y13822509112@126.com>
2023-02-05 15:00:44 +0800

    Implement a doubly linked list of basic data structures

---
+++ src/include/ynix/list.h
@@ -0,0 +1,29 @@
+#ifndef YNIX_LIST_H
+#define YNIX_LIST_H
+
+#include "types.h"
+
+// 侵入式链表
+typedef struct list_node_t {
+    struct list_node_t* prev;
+    struct list_node_t* next;
+} list_node_t;
+
+typedef struct list_t {
+    list_node_t head;
+    list_node_t tail;
+} list_t;
+
+void list_init(list_t* list);
+void list_insert_before(list_node_t* anchor, list_node_t* node);
+void list_insert_after(list_node_t* anchor, list_node_t* node);
+void list_push(list_t* list, list_node_t* node);
+list_node_t* list_pop(list_t* list);
+void list_pushback(list_t* list, list_node_t* node);
+list_node_t* list_popback(list_t* list);
+bool list_search(list_t* list, list_node_t* node);
+void list_remove(list_node_t* node);
+bool list_empty(list_t* list);
+u32 list_size(list_t* list);
+
+#endif
---
+++ src/kernel/list.c
@@ -0,0 +1,85 @@
+#include "../include/ynix/list.h"
+
+void list_init(list_t* list) {
+    list->head.prev = NULL;
+    list->tail.next = NULL;
+    list->head.next = &list->tail;
+    list->tail.prev = &list->head;
+}
+
+void list_insert_before(list_node_t* anchor, list_node_t* node) {
+    node->prev = anchor->prev;
+    node->next = anchor;
+
+    anchor->prev->next = node;
+    anchor->prev = node;
+}
+
+void list_insert_after(list_node_t* anchor, list_node_t* node) {
+    node->next = anchor->next;
+    node->prev = anchor;
+
+    anchor->next->prev = node;
+    anchor->next = node;
+}
+
+void list_push(list_t* list, list_node_t* node) {
+    assert(!list_search(list, node));
+    list_insert_after(&list->head, node);    
+}
+
+list_node_t* list_pop(list_t* list) {
+    assert(!list_empty(list));
+
+    list_node_t *node = list->head.next;
+    list_remove(node);
+
+    return node;
+}
+
+void list_pushback(list_t* list, list_node_t* node) {
+    assert(!list_search(list, node));
+    list_insert_before(&list->tail, node);
+}
+
+list_node_t* list_popback(list_t* list) {
+    assert(!list_empty(list));
+
+    list_node_t *node = list->tail.prev;
+    list_remove(node);
+
+    return node;
+}
+
+bool list_search(list_t* list, list_node_t* node) {
+    list_node_t* cur = list->head.next;
+    while(cur != &list->tail) {
+        if(cur == node) {
+            return true;
+        }
+        cur = cur->next;
+    }
+    return false;
+}
+
+void list_remove(list_node_t* node) {
+    assert(node->prev && node->next);
+    node->prev->next = node->next;
+    node->next->prev = node->prev;
+    node->prev = NULL;
+    node->next = NULL;
+}
+
+bool list_empty(list_t* list) {
+    return list->head.next == &list->tail;
+}
+
+u32 list_size(list_t* list) {
+    u32 size = 0;
+    list_node_t* cur = list->head.next;
+    while(cur != &list->tail) {
+        size ++;
+        cur = cur->next;
+    }
+    return size;
+}
